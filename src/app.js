5560d684ffbb23a3979a3881a63e99be597bb338
Gregori-Rivera <gregori.rivera@gmail.com>
2022-11-04 00:53:54 +0000

    codigo base

--- src/app.js
+++ src/app.js
@@ -7,5 +7,39 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "your", "me", "they"];
+  let adj = ["-great", "-big", "-small", "-medium"];
+  let name = ["-anaconda", "-piton", "-cobra", "-cascabel"];
+  let domain = [
+    ".com",
+    ".net",
+    ".ve",
+    ".us",
+    ".es",
+    ".co",
+    ".el",
+    ".ra",
+    ".da",
+    ".on"
+  ];
+
+  for (let w = 0; w < pronoun.length; w++) {
+    for (let x = 0; x < adj.length; x++) {
+      for (let y = 0; y < name.length; y++) {
+        for (let z = 0; z < domain.length; z++) {
+          if (name[y].endsWith(domain[z])) {
+            console.log(
+              pronoun[w] +
+                adj[x] +
+                name[y].slice(0, -1 * domain[z].length) +
+                "." +
+                domain[z]
+            );
+          } else {
+            console.log(pronoun[w] + adj[x] + name[y] + ".com");
+          }
+        }
+      }
+    }
+  }
 };
