abfa2d89df258477c70cbd3a83af58af5c866a02
Gregori-Rivera <gregori.rivera@gmail.com>
2022-11-04 21:19:49 +0000

    arreglado .com

--- src/app.js
+++ src/app.js
@@ -10,18 +10,7 @@ window.onload = function() {
   let pronoun = ["the", "your", "me", "they"];
   let adj = ["-great", "-big", "-small", "-medium"];
   let name = ["-anaconda", "-piton", "-cobra", "-cascabel"];
-  let domain = [
-    ".com",
-    ".net",
-    ".ve",
-    ".us",
-    ".es",
-    ".co",
-    ".el",
-    ".ra",
-    ".da",
-    ".on"
-  ];
+  let domain = ["com", "ve", "net", "us", "es", "co", "el", "ra", "da", "on"];
 
   for (let w = 0; w < pronoun.length; w++) {
     for (let x = 0; x < adj.length; x++) {
